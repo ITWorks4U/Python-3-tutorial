77d1802e0b8d5208e90661b2f94d042fbec182e7
ITWorks4U <52790370+ITWorks4U@users.noreply.github.com>
2023-05-12 05:31:34 +0200

    first steps into python programming

---
+++ 01_basic_usage/commentaries.py
@@ -0,0 +1,30 @@
+#	commentaries allows you to documenting your code or reading foreign code
+#	to understand that.
+
+#	There're two kinds of commentaries:
+#	-	single line (this one)
+#	-	multiple lines
+
+#	single line commentaries allows you to comment anything in >>this<< line
+
+'''
+	multiple commentaries
+	allows you to comment multiple lines
+'''
+
+"""
+	this is also valid for multiple
+	commentaries
+
+	important: the types of the starting
+	and ending quotas must be equal and should not
+	be inside of this commentary
+"""
+
+#"""
+#	a bad example of commentary; let's say,
+#	you're using """ inside of your commentary,
+#	then pyton declares this as an error on runtime
+#"""
+
+#	commentaries aren't in use for the interpreter
