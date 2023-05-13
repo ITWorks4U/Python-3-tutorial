a743af384f499661724e5972eb4b287dc20c72c5
ITWorks4U <itworks4U@gmail.com>
2023-05-13 10:17:43 +0200

    file IO

---
+++ 07_IO/file IO/reading.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+"""
+	Reading from a file.
+
+	It works only, if the required
+	file exists, you've granted access
+	and this file is able to use,
+	otherwise an error occurs.
+
+	To read a file use open() function:
+	open(file, mode, buffering, encoding, errors, newline, closefd, opener), where:
+		-	file		:=	file to use
+		-	mode		:=	how to use that file
+			'r'	-	reading (default) <=> file must exist
+			'w'	-	writing <=> truncates content of file, if
+					exsiting, otherwise create a new file
+			'x'	-	opening for exclusive creation;
+					fails, if the file already exists
+			'a'	-	appending content to a file, if exists,
+					otherwise creating a new file and fill it with content
+			't'	-	open in text mode (default)
+			'b'	-	open in binary mode
+			'+'	-	open a file for updating (reading and writing)
+		-	buffering	:=	used for setting buffering policy
+		-	encoding	:=	the encoding format (e. g. ASCII, UTF-7, UTF-8, ...)
+		-	errors		:=	string specifying how to handle encoding/decoding errors
+		-	newline		:=	how newlines mode works (available values: None, ' ', '\n', 'r', and '\r\n')
+		-	closefd		:=	True by default; if not given, an exception will be raised
+		-	opener		:=	a custom opener; must return an open file descriptor
+
+	you're also welcome to use one of its
+	overloaded functions, too
+
+	see also: https://www.programiz.com/python-programming/methods/built-in/open
+"""
+#	---------------
+#	first option to read a file:
+#	---------------
+source = open("lorem_ipsum.txt", "r")
+
+#	print line by line
+for line in source:
+	print(line)
+#end for
+
+#	closes the file stream
+source.close()
+print("Source is closed? ", source.closed)
+print("---------------")
+
+#	---------------
+#	second option to
+#	read a file
+#	---------------
+source = open("lorem_ipsum.txt", mode="r", encoding="ascii")
+
+#	using read() function instead
+print(source.read())
+source.close()
+print("Source is closed? ", source.closed)
+print("---------------")
+
+#	---------------
+#	third option to
+#	read a file:
+#	---------------
+#	third option to read a file:
+#	using with command => automatically closes the
+#	file stream after completing with your operations
+with open("lorem_ipsum.txt", "r") as source:
+	print(source.read())
+# end with
+print("Source is closed? ", source.closed)
---
+++ 07_IO/keyboard input/console_IO.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+"""
+	Reading from keyboard, storing that
+	content to a variable, etc.
+
+	Every input is always a string (word), so
+	it will cause an error on runtime if you're
+	trying to use a different usage, e. g. interpreting
+	this input as a number, even it's not a number.
+"""
+
+#	-------------------
+#	enter anything, which is going
+#	to convert to a string by default
+#	-------------------
+buffer = input("enter anything: ")
+print(f'Your input: {buffer}, its a type of "{type(buffer)}", and it has a length of {len(buffer)} characters.')
+
+#	-------------------
+#	interpreting the input
+#	as an integer => make
+#	sure, this is possible
+#
+#	in some later sections, you'll
+#	learn some mechanics to
+#	handle such kind of errors
+#	-------------------
+print("-------------------")
+buffer = input("give me a number: ")
+print(f'Your input: {buffer}, its a type of "{type(buffer)}", and it has a length of {len(buffer)} characters.')
+
+#	try to convert this input to an intege, IF possible
+buffer = int(buffer)
+print(f"after successfully converting your input ({buffer}) has now this type: {type(buffer)}")
+
+print("-------------------")
+#	-------------------
+#	fun fact: every decimal number with
+#	leading zeros won't use the leading zeros
+#	unless you're using that format: 0xd
+#	leading with zeros x times ->
+#	x := number of zeros to use
+#	-------------------
+
+number = 00000
+print("default number: ", number)
+print("formatted version #1: %05d" % number)
+print("formatted version #2: {0:05d}".format(number))
+print(f"formatted version #3: {number:05d}")
+
+#	have a space of x chars before the output starts
+print("Don't be confused with that form: {0:5d}".format(number))
+
+print("-------------------")
+#	-------------------
+#	entering two "numbers" and calculating
+#	the product of them
+#	-------------------
+buffer = input("give me two numbers: ")
+
+"""
+	split() splits the input into a list
+	of n elements depending on the required
+	token
+
+	in contrast to other languages it's not
+	possible to use multiple tokens (yet)
+
+	regular expressions are often used
+	for multiple tokens
+
+	in that case we're using each comma
+	as a token
+"""
+buffer = buffer.split(",")
+a = int(buffer[0])
+b = int(buffer[1])
+print("product of a*b is ", (a*b))
+
+#	-------------------
+#	... or use the short version:
+#	-------------------
+a,b = [int(i) for i in input("give me two numbers: ").split(",")]
+print("product of a*b is ", (a*b))
