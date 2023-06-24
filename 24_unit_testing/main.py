287bf5b1f176f04155ed3002805b6a0907dd1fe6
ITWorks4U <itworks4U@gmail.com>
2023-06-24 10:46:01 +0200

    unit testing examples

---
+++ 24_unit_testing/main.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+"""
+	Unit testing allows you to test
+	specific scenarios for your project.
+
+	In math.py we're using simple
+	arithmetic operations, thus
+	we can assume an expected result
+	and also specific errors, like
+	division by zero.
+
+	The result of test cases may be:
+	.	:=	executed successfully
+	x	:=	expected a failure
+	u	:=	unexpected successes
+	S	:=	skipped that test
+	F	:=	failed test
+"""
+
+#	gain access to unit testing
+import unittest as ut
+
+#	using everything from our math module
+import mathTest as m
+
+#	-------------
+#	Test cases.
+#	-------------
+class TestingMath(ut.TestCase):
+	#	-------------
+	#	in use to create required
+	#	objects, variables, ...
+	#	before a test begins
+	#	-------------
+	def setUp(self):
+		self.bm = m.BasicMath()
+	#end init
+
+	#	-------------
+	#	clean up between each
+	#	test to make sure, that
+	#	no orphan object exists
+	#	-------------
+	def tearDown(self):
+		del self.bm
+	#end disposing
+
+	#	-------------
+	#	testing, if 3 + 4 = 7
+	#	-------------
+	def testAdd(self):
+
+		#	-------------
+		#	assert := we're assuming, that a certain
+		#	condition shall be true
+		#	-------------
+		#	if true, no error appears here,
+		#	if false, an AssertionError appears, which
+		#	marks this method as failed
+		#	-------------
+		#	Since an AssertionError may appear, we're not
+		#	forced to handle this exception. It will be
+		#	handled automatically.
+		#	-------------
+		self.assertEqual(self.bm.add(3,4), 7, '3 + 4 = 7!')
+	#end method
+
+	#	-------------
+	#	Division by zero fails and this test will also
+	#	fail, however, we can handle this failure,
+	#	when we're expected that.
+	#	-------------
+	#	use @[module].expectedFailure to handle that
+	#	-------------
+	@ut.expectedFailure
+	def testDivideByZero(self):
+		self.assertEqual(self.bm.div(15,0), 0, 'Wait a minute...!')
+	#end method
+
+	#	-------------
+	#	Testing, that this raises our math exception class,
+	#	when we're using arguments, which are not decimal (int).
+	#	-------------
+	def testWrongArguments(self):
+		bm = m.BasicMath()
+
+		with self.assertRaises(m.MathException):
+			bm.add('howdy', 'ho')
+		#end with
+	#end method
+
+	#	-------------
+	#	Skipping this test for a later purpose.
+	#	-------------
+	def skipTest1(self):
+		with self.skipTest("skipped at the moment"):
+			bm.add('3', 9)
+		#end with
+	#end method
+
+	#	-------------
+	#	Also possible to skip a test.
+	#	-------------
+	@ut.skip("demonstrating skipping")
+	def skipTest2(self):
+		self.fail("epic fail")
+	#end method
+#end class
+
+if __name__ == '__main__':
+	#	launching the unit tests
+	ut.main()
+#end if
---
+++ 24_unit_testing/mathTest.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+
+"""
+	Offers simple arithmetic
+	operations, which are going
+	to test by using unit tests.
+"""
+
+#	-------------
+#	custom exception class
+#	-------------
+class MathException(Exception):
+	def __init__(self, errorMessage: str) -> None:
+		self.__errMsg = errorMessage
+	#end constructor
+
+	def getErrorMessage(self) -> str:
+		return self.__errMsg
+	#end method
+#end class
+
+class BasicMath():
+	def add(self, a: int, b: int) -> int:
+		if not isinstance(a, int) or not isinstance(b, int):
+			raise MathException('illegal argument usage; int is required')
+		#end if
+
+		return a+b
+	#end method
+
+	def sub(arg1: int, arg2: int) -> int:
+		if not isinstance(arg1, int) or not isinstance(arg2, int):
+			raise MathException('one of the arguments is not decimal')
+		#end if
+	
+		return arg1 - arg2
+	#end function
+	
+	def mul(arg1: int, arg2: int) -> int:
+		if not isinstance(arg1, int) or not isinstance(arg2, int):
+			raise MathException('one of the arguments is not decimal')
+		#end if
+	
+		return arg1 * arg2
+	#end function
+	
+	def div(arg1: int, arg2: int) -> int:
+		if not isinstance(arg1, int) or not isinstance(arg2, int):
+			raise MathException('one of the arguments is not decimal')
+		#end if
+	
+		if arg2 == 0:
+			raise MathException('division by zero is not allowed')
+		#end if
+	
+		return arg1 / arg2
+	#end function
+#end class
+
+
+
+"""
+#	-------------
+#	functions to test
+#	-------------
+def add(arg1: int, arg2: int) -> int:
+	if not isinstance(arg1, int) or not isinstance(arg2, int):
+		raise MathException('one of the arguments is not decimal')
+	#end if
+
+	return arg1 + arg2
+#end function
+
+def sub(arg1: int, arg2: int) -> int:
+	if not isinstance(arg1, int) or not isinstance(arg2, int):
+		raise MathException('one of the arguments is not decimal')
+	#end if
+
+	return arg1 - arg2
+#end function
+
+def mul(arg1: int, arg2: int) -> int:
+	if not isinstance(arg1, int) or not isinstance(arg2, int):
+		raise MathException('one of the arguments is not decimal')
+	#end if
+
+	return arg1 * arg2
+#end function
+
+def div(arg1: int, arg2: int) -> int:
+	if not isinstance(arg1, int) or not isinstance(arg2, int):
+		raise MathException('one of the arguments is not decimal')
+	#end if
+
+	if arg2 == 0:
+		raise MathException('division by zero is not allowed')
+	#end if
+
+	return arg1 / arg2
+#end function
+"""
