0b4e8e6e4a1622987ed55dc33669b26c8dd4ac8e
ITWorks4U <itworks4U@gmail.com>
2023-06-28 06:20:28 +0200

    have fun with regular expressions

--- 25_regular_expressions/check if number.py
+++ 25_regular_expressions/check if number.py
@@ -18,6 +18,29 @@
 	passes trough a defined filter or not.
 	=>	In that case no exception handling is necessary.
 
+	A regular expression can only be used on words!
+	Since a word with a length n characters (n = 0, 1, 2, ..., k-1)
+	is given, a regular expression can be used to define a filter
+	to check, if this certain word passes the filter or not.
+
+	Typicial filters are:
+	-	^[A-Za-z0-9]+$
+		-	any word with at least one character
+		-	may contain a character between A-Z, a-z or 0-9
+	
+	-	^$
+		-	empty words only (have a length of 0 characters)
+	
+	-	^(0x|0X)?([A-Fa-f0-9]+)(H|h)?$
+		-	checking, if a hexadecimal expression is given
+		-	starting with 0x or 0X
+		-	followed by A-F, a-f or 0-9 multiple times
+		-	may contain h or H at the end
+
+	When you're trying to use other objects, like numbers, collections,
+	or else, the regular expression check usually fails and must be
+	covered with an exception handling.
+
 	For more details, take a look to this video:
 	https://www.youtube.com/watch?v=vvFPWHHto00
 
